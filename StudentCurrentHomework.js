12739e147e98277a6c30c249006b1a56fb6de6a8
finleyjg <snh22fpu@uea.ac.uk>
2024-12-05 22:44:25 +0000

    form n that

--- StudentCurrentHomework.js
+++ StudentCurrentHomework.js
@@ -1,5 +1,17 @@
+// upload
 const uploadButton = document.getElementById('uploadButton');
 
 uploadButton.addEventListener('click', () => {
     alert('Upload button clicked!');
+});
+
+// direct to assignment form
+document.addEventListener("DOMContentLoaded", () => {
+    const doAssignmentButton = document.querySelector(".doAssignmentButton");
+
+    // Add event listener to the button
+    doAssignmentButton.addEventListener("click", () => {
+        // Redirect to AssignmentForm.html
+        window.location.href = "AssignmentForm.html";
+    });
 });
