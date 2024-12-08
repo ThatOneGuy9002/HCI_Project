982a897cec2db72bdd889bab571cf0ac163d1845
ThatOneGuy9002 <connortfreeman@outlook.com>
2024-12-08 19:31:10 +0000

    added graph page and password system

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
+const studentCredentials = {
+    username: "student",
+    password: "password"
+}
 
-    const studentLoginButton = document.getElementById('studentLoginButton');
-    const staffLoginButton = document.getElementById('staffLoginButton');
+const staffCredentials = {
+    username: "staff",
+    password: "password"
+}
 
-    studentLoginButton.addEventListener('click', function () {
-        window.location.href = 'StudentHome.html';
-    });
+const loginForm = document.getElementById("login-form");
+const errorMessage = document.getElementById("errorMessage");
 
-    staffLoginButton.addEventListener('click', function () {
-        window.location.href = 'StaffHome.html';
-    });
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (username === studentCredentials.username && password === studentCredentials.password) {
+        window.location.href = "StudentHome.html";
+    }
+    else if (username === staffCredentials.username && password === staffCredentials.password){
+        window.location.href = "StaffHome.html";
+    }
+    else {
+        errorMessage.textContent = "Incorrect username or password.";
+        usernameInput.classList.add("input-error");
+        passwordInput.classList.add("input-error");
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+    }
 });
+
+
