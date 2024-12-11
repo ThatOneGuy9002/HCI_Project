461ea88ffd2462ae24ff41dd591abaf76ca8322f
ThatOneGuy9002 <connortfreeman@outlook.com>
2024-12-11 19:54:26 +0000

    janitor duty

--- scripts/StudentCurrentHomework.js
+++ scripts/StudentCurrentHomework.js
@@ -1,17 +1,13 @@
-// upload
 const uploadButton = document.getElementById('uploadButton');
 
 uploadButton.addEventListener('click', () => {
     alert('Upload button clicked!');
 });
 
-// direct to assignment form
 document.addEventListener("DOMContentLoaded", () => {
     const doAssignmentButton = document.querySelector(".doAssignmentButton");
 
-    // Add event listener to the button
     doAssignmentButton.addEventListener("click", () => {
-        // Redirect to AssignmentForm.html
         window.location.href = "AssignmentForm.html";
     });
 });
