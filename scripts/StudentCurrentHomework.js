7d9b7e8fab9ca1b62c423e50d2ea6ee3a81fe3bc
finleyjg <snh22fpu@uea.ac.uk>
2024-12-11 18:42:02 +0000

    Update StudentCurrentHomework.js

--- scripts/StudentCurrentHomework.js
+++ scripts/StudentCurrentHomework.js
@@ -5,13 +5,10 @@ uploadButton.addEventListener('click', () => {
     alert('Upload button clicked!');
 });
 
-// direct to assignment form
 document.addEventListener("DOMContentLoaded", () => {
     const doAssignmentButton = document.querySelector(".doAssignmentButton");
 
-    // Add event listener to the button
     doAssignmentButton.addEventListener("click", () => {
-        // Redirect to AssignmentForm.html
         window.location.href = "AssignmentForm.html";
     });
 });
